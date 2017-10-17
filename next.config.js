de9a8228bc8914fee085b76ec6fdbe0297ea8174
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-10-17 12:55:34 +0600

    Add next.config file

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// const path = require('path')
+
+module.exports = {
+  webpack: (config, { dev }) => {
+    // config.resolve.modules = [path.resolve(__dirname, "components"), "node_modules"]
+    return config
+  },
+  webpackDevMiddleware: (config) => {
+    return config
+  }
+}
