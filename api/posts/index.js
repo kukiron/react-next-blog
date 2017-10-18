0834b3b5af2714e3dbab8e62e21a4495ea8724b7
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-10-18 06:30:56 +0600

    Change api endpoints to get akceed blog posts

--- api/posts/index.js
+++ api/posts/index.js
@@ -1,9 +1,9 @@
 import fetch from 'isomorphic-fetch'
 
 export function getPosts () {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
+  return fetch('https://www.akceed.com/wp-json/wp/v2/posts')
 }
 
 export function getPost (slug) {
-  return fetch(`https://jsonplaceholder.typicode.com/posts?title=${slug}`)
+  return fetch(`https://www.akceed.com/wp-json/wp/v2/posts?title=${slug}`)
 }
